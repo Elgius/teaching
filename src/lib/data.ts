34f2d505181003c955933d57adaacc78cfe5cd30
Elgius <114131653+Elgius@users.noreply.github.com>
2024-11-20 10:32:22 +0500

    0.1.3 course tables stable

---
+++ src/lib/data.ts
@@ -0,0 +1,71 @@
+export const courses = [
+  {
+    courseId: "1",
+    name: "Algebra for Beginners",
+    subject: "Mathematics",
+    amount: "MVR 200",
+    description:
+      "Learn the fundamentals of algebra, covering variables, equations, and functions.",
+    duration: "12 weeks",
+    level: "Beginner",
+    rating: 4.5,
+    reviews: [
+      "Great course for those who struggle with math!",
+      "Helpful explanations and examples.",
+    ],
+    stream: ["High school students", "College freshmen"],
+  },
+  {
+    courseId: "2",
+    name: "Introduction to Programming",
+    subject: "Computer Science",
+    amount: "MVR 250",
+    description:
+      "Discover the world of programming through hands-on exercises and projects.",
+    duration: "16 weeks",
+    level: "Advanced",
+    rating: 4.8,
+
+    stream: ["Career changers", "Tech enthusiasts"],
+  },
+  {
+    courseId: "3",
+    name: "Advanced Calculus",
+    subject: "Mathematics",
+    amount: "MVR 300",
+    description:
+      "Delve into complex mathematical concepts and their real-world applications.",
+    duration: "20 weeks",
+    level: "Ordinary",
+
+    rating: 4.9,
+
+    stream: ["Mathematics majors", "Engineering students"],
+  },
+  {
+    courseId: "4",
+    name: "Poissons Theorem",
+    subject: "Statistics",
+    amount: "MVR 200",
+    description: "Learn to use Poissons Theorem",
+    duration: "10 weeks",
+    level: "Ordinary",
+    rating: 4.6,
+
+    stream: ["Business analysts", "Data scientists"],
+  },
+
+  {
+    courseId: "5",
+    name: "Econometrics",
+    subject: "Economics",
+    amount: "MVR 220",
+    description:
+      "Learn how to apply statistical methods to economic data analysis and modeling.",
+    duration: "14 weeks",
+    level: "Advanced",
+    rating: 4.7,
+
+    stream: ["Economics majors", "Business analysts"],
+  },
+];
